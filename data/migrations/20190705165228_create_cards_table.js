c26c0f66bd0fe665e51f30fb24c067085bc309a6
Frank Martinez <frankie.martinez@gmail.com>
2019-07-06 15:24:07 -0700

    Fixed variations bug

--- data/migrations/20190705165228_create_cards_table.js
+++ data/migrations/20190705165228_create_cards_table.js
@@ -60,7 +60,7 @@ exports.up = function(knex, Promise) {
 
         cards.text('types');        
 
-        cards.string('variations', 255);
+        cards.text('variations');
 
         cards.string('watermark', 255);
 
