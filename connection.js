81795f46fe6666a363629edad7c223654dbdd2ab
Frank Martinez <frankie.martinez@gmail.com>
2019-08-03 18:29:30 -0700

    Playing around with Node syntax

---
+++ connection.js
@@ -0,0 +1,12 @@
+// // require('dotenv').config(); // Import dotenv config
+// // const dotenv = require("../server.js");
+// // const enviro = 'development';
+
+// const enviro = process.env.NODE_DB_ENV || 'development';
+
+
+// const config = require('../knexfile.js')[enviro];
+// const knex = require('knex')(config);
+
+
+// module.exports = knex;
